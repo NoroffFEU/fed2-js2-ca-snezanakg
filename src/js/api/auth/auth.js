52dd5b83d684f1a36128a910037fd63c1aee4a38
snezanakg <142974537+snezanakg@users.noreply.github.com>
2025-05-02 17:06:46 +0200

    just a test

--- src/js/api/auth/auth.js
+++ src/js/api/auth/auth.js
@@ -8,7 +8,7 @@ export async function loginUser({ email, password }) {
   const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.errors?.[0]?.message || "Login failed.");
+    throw new Error(data.errors?.[0]?.message || "Login failed");
   }
 
   return data;

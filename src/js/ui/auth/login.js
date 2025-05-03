11511de5bf2a517e9cd1c776f30a18b2ff09721e
snezanakg <142974537+snezanakg@users.noreply.github.com>
2025-05-04 01:32:53 +0200

    huge problem with redirection after login

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -10,6 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
+    console.log("✅ Login successful, redirecting to /feed.html...");
+window.location.href = "/feed.html";
+
+
     try {
       await loginUser(email, password);
       window.location.href = "/feed.html"; // match exact path in project root
